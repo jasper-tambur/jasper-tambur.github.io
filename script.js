015f43dd20e08b397b1b7eccb41a5f6788987c3d
jasper-tambur <jasper.tambur@ametikool.ee>
2023-04-26 17:43:29 +0300

    rework
    võidetav, värvid

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-const secretWordDiv = document.getElementById('secret-word');
-const wrongLettersDiv = document.getElementById('wrong-letters');
+const alphabet = ["A", "B", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "Š", "Z", "Ž", "T", "U", "V", "Õ", "Ä", "Ö", "Ü"];
+const secretWordDiv = document.getElementById("secret-word");
 const wrongLetterDiv = document.getElementById("wrong-word");
-const livesDiv = document.getElementById('lives');
+const livesDiv = document.getElementById("lives");
+const stickImg = document.querySelectorAll(".img");
 let inCorrectLetter = [];
+let secretWord = '';
 let lives = 9;
 let strike = 0;
-let secretWord = '';
+let isGameOver = false;
 
 fetch('words_hangman.txt')
 .then(response => response.text())
 .then(data => {
-    words = data.split('\n');
-
+    words = data.trim().split(/\s+/);
     secretWord = words[Math.floor(Math.random() * words.length)];
-
     initGameBoard();
-
 });
-function initGameBoard () {
-    for ( let i = 0; i < secretWord.length; i++ ) {
+
+function initGameBoard() {
+    for (let i = 0; i < secretWord.length; i++) {
         const charSpan = document.createElement('span');
         charSpan.setAttribute('id', 'char-' + i);
-        charSpan.innerText = '_ ';
+        charSpan.innerText = "_";
         secretWordDiv.appendChild(charSpan);
+    };
+}
+
+function endGame(isWin) {
+    isGameOver = true;
+    if (isWin) {
+        livesDiv.style.color = "green";
+        livesDiv.innerHTML = "Õige vastus!";
+    } else {
+        document.getElementById("hangman-9").style.display = "block";
+        livesDiv.innerHTML = 'Mäng läbi! Proovi uuesti.<br>' + secretWord;
     }
 }
 
-
-const alphabet = ['A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'Š', 'Z', 'Ž', 'T', 'U', 'V', 'Õ', 'Ä', 'Ö', 'Ü'];
-let inncorrectLetters = [];
-for (let i = 0; i < secretWord.length; i++) {
-    const charSpan = document.createElement('span');
-    charSpan.setAttribute('id', 'char-' + i);
-    charSpan.innerText = '_ ';
-    secretWordDiv.appendChild(charSpan);
-    //<span id="char-0"></span>
-    console.log(secretWord[i]);
-
-
-}
 document.addEventListener('keydown', (e) => {
+    if (isGameOver) return;
     const charGuess = e.key.toUpperCase();
     if (alphabet.includes(charGuess) && lives != 0) {
 
@@ -57,29 +56,17 @@ document.addEventListener('keydown', (e) => {
             wrongLetterDiv.innerHTML += charGuess;
             lives--;
             strike++;
-            } if (lives > 0) {
+            if (lives == 0) {
+                endGame(false);
+            } else {
                 livesDiv.innerText = lives;
                 if (strike > 0) {
                     let stickDiv = document.getElementById("hangman-" + strike);
                     stickDiv.style.display = "block";
                 }
-            } else {
-                livesDiv.innerHTML = 'MÄNG LÄBI!<br>' + secretWord;
-                let stickDiv = document.getElementById("hangman-9");
-                stickDiv.style.display = "block";
-        }
-        } else {
-            let isWordComplete = true;
-            for (let i = 0; i < secretWord.length; i++) {
-                const charSpan = document.getElementById('char-' + i);
-                if (charSpan.innerText == '_ ') {
-                    isWordComplete = false;
-                    break;
-                }
             }
-            if (isWordComplete) {
-                secretWordDiv.innerHTML = secretWord.toUpperCase();
-                livesDiv.innerHTML = 'SINA VÕITSID!';
-            } 
+        } else if (!secretWordDiv.innerText.includes("_")) {
+            endGame(true);
+        }
     }
 });
